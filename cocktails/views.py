8ddba1fbf175b180e6feee472538f927b13dc4bb
Eiji224 <andremaps06@gmail.com>
2026-01-04 00:46:31 +0500

    created index page

--- cocktails/views.py
+++ cocktails/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Cocktail
+
+def index(request):
+    return render(request, 'cocktails/index.html')
