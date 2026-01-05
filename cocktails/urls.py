09d01fb0eeadd68e89ab331caea623efa08d83f5
Eiji224 <andremaps06@gmail.com>
2026-01-05 14:59:00 +0500

    created cocktail page

--- cocktails/urls.py
+++ cocktails/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cocktails/<int:page>', views.browse_cocktails, name='cocktails')
 ]
--- cocktails/views.py
+++ cocktails/views.py
@@ -3,5 +3,22 @@ from django.shortcuts import render
 
 from .models import Cocktail
 
+def chunked(iterable, size):
+    for i in range(0, len(iterable), size):
+        yield iterable[i:i + size]
+
 def index(request):
     return render(request, 'cocktails/index.html')
+
+def browse_cocktails(request, page: int):
+    page_size = 20
+    columns = 4
+
+    offset = (page - 1) * page_size
+
+    cocktails = Cocktail.objects.order_by('id')[offset : offset + page_size]
+    cocktail_rows = chunked(cocktails, columns)
+    context = {
+        'cocktail_rows': cocktail_rows,
+    }
+    return render(request, 'cocktails/cocktails.html', context)
