ff5a4028baa812977e7c7f20132aa2692fc63797
Eiji224 <andremaps06@gmail.com>
2026-01-10 13:07:35 +0500

    Added ingredient page and refactored templates

--- cocktails/urls.py
+++ cocktails/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('cocktails/<int:page>', views.browse_cocktails, name='cocktails')
+    path('cocktails/<int:page>', views.browse_cocktails, name='cocktails'),
+    path('ingredients/<int:page>', views.browse_ingredients, name='ingredients'),
 ]
--- cocktails/views.py
+++ cocktails/views.py
@@ -1,26 +1,41 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
-from .models import Cocktail
+from .models import Cocktail, Ingredient
+
+BROWSE_PAGES_INFO = {
+    'items_per_page': 20,
+    'items_per_row': 4,
+}
+
+def group_into_rows(items, per_row):
+    items = list(items)
+    return [items[i:i + per_row] for i in range(0, len(items), per_row)]
+
 
 def index(request):
     return render(request, 'cocktails/index.html')
 
 def browse_cocktails(request, page: int):
-    elements_qty = 20
-    items_per_row = 4
-
     cocktails = Cocktail.objects.order_by('id')
-    paginator = Paginator(cocktails, elements_qty)
+    paginator = Paginator(cocktails, BROWSE_PAGES_INFO.get('items_per_page'))
     page_obj = paginator.get_page(page)
 
-    def group_into_rows(items, per_row):
-        items = list(items)
-        return [items[i:i + per_row] for i in range(0, len(items), per_row)]
-
-    rows = group_into_rows(page_obj.object_list, items_per_row)
+    rows = group_into_rows(page_obj.object_list, BROWSE_PAGES_INFO.get('items_per_row'))
 
     return render(request, 'cocktails/cocktails.html', {
+        'page_obj': page_obj,
+        'rows': rows,
+    })
+
+def browse_ingredients(request, page: int):
+    ingredients = Ingredient.objects.order_by('id')
+    paginator = Paginator(ingredients, BROWSE_PAGES_INFO.get('items_per_page'))
+    page_obj = paginator.get_page(page)
+
+    rows = group_into_rows(page_obj.object_list, BROWSE_PAGES_INFO.get('items_per_row'))
+
+    return render(request, 'cocktails/ingredients.html', {
         'page_obj': page_obj,
         'rows': rows,
     })
