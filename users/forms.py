fc3e4ccb21eeeae1d91752b3374771bf4cf2a1a2
Eiji224 <andremaps06@gmail.com>
2026-01-15 00:46:48 +0500

    Register and Login

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,38 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
-
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs.update({
+                'class': 'form-control form-control-lg',
+                'placeholder': self.fields[field_name].label or field_name.capitalize()
+            })
+
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'Enter your username',
+            'autofocus': True
+        })
+        self.fields['email'].widget.attrs.update({
+            'placeholder': 'your.email@example.com'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': 'Create a strong password'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': 'Confirm your password'
+        })
+
+        self.fields['password1'].help_text = (
+            '<small class="form-text text-white">'
+            'Your password must contain at least 8 characters, '
+            'not be commonly used, and not be entirely numeric.'
+            '</small>'
+        )
+        self.fields['password2'].help_text = ''
