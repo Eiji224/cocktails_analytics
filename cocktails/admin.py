526c9f1a23c128fa59d0145eb97cda3b4e2483d6
Eiji224 <andremaps06@gmail.com>
2026-01-19 18:21:43 +0500

    added models to admin

--- cocktails/admin.py
+++ cocktails/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cocktail, Ingredient, CocktailIngredient, FavouriteCocktail
+
+
+@admin.register(Cocktail)
+class CocktailAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_alcoholic", "image_url")
+    search_fields = ("name", "id")
+    list_filter = ("is_alcoholic",)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "abv", "image_url")
+    search_fields = ("name", "id")
+    list_filter = ("abv",)
+
+
+@admin.register(CocktailIngredient)
+class CocktailIngredientAdmin(admin.ModelAdmin):
+    list_display = ("id", "cocktail", "ingredient", "ingredient_measure")
+    search_fields = ("cocktail__name", "ingredient__name", "cocktail__id", "ingredient__id")
+    autocomplete_fields = ("cocktail", "ingredient")
+
+
+@admin.register(FavouriteCocktail)
+class FavouriteCocktailAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "cocktail")
+    search_fields = ("user__username", "user__email", "cocktail__name", "cocktail__id")
+    autocomplete_fields = ("user", "cocktail")
